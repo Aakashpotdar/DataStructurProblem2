51f7390e058b3c1b0c2bba92d9bb1459a39fc7e8
aakashpotdar <potdaraakash@gmail.com>
2021-11-25 00:58:30 +0530

    validation done

---
+++ ArithmaticExprestionValidation/ArithmaticExprestionValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArithmaticExpressionValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)";
+
+            validationClass obj = new validationClass();
+            if (obj.isValidExpression(expression))
+            {
+                Console.WriteLine("the expression is valid");
+            }
+            else
+            {
+                Console.WriteLine("the expression is not valid");
+            }
+        }
+        
+    }
+}
---
+++ ArithmaticExprestionValidation/ArithmaticExprestionValidation/validationClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArithmaticExpressionValidation
+{
+    public class validationClass
+    {
+        public static bool isAnOperator(char c)
+        {
+            return (c == '*' || c == '/' || c == '+' || c == '-' || c == '%');
+        }
+        public bool isValidExpression(String expression)
+        {
+            expression = expression.Trim();
+            
+                char[] s = expression.ToCharArray();
+            
+            if (isAnOperator(s[0]) || isAnOperator(s[s.Length - 1]))
+                    {
+                return false;
+            }
+            int opning= 0;
+            int closing = 0;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    opning++;
+
+                    if (i == expression.Length - 1) 
+                    {
+                        return false;
+                    }
+                    if (s[i + 1] == ')')
+                    {
+                        return false;
+                    }
+                }
+                if (s[i] == ')')
+                {
+                    closing++;
+                    if (i == 0) 
+                    {
+                        return false; 
+                    }
+                    if (opning<closing)
+                    {
+                        return false;
+                    }
+                }
+                if (isAnOperator(s[i]))
+                {
+                    if (s[i - 1] == '(' || s[i + 1] == ')' || isAnOperator(s[i + 1]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return (opning==closing);
+        }
+    }
+}
