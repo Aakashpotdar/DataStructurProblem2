c0ab4198361890292d9035beb5ee7ee8f5c876d4
aakashpotdar <potdaraakash@gmail.com>
2021-11-24 23:50:14 +0530

    done

---
+++ OrderdList/OrderdList/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderdList
+{
+    class LinkedList
+    {
+        Node head;
+        public void add(string number)
+        {
+            Node node = new Node(number);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+        }
+        internal void Display()
+        {
+            Node temp1 = this.head;
+            if (this.head == null)
+            {
+                Console.WriteLine("the Queue is empty");
+                return;
+            }
+            while (temp1 != null)
+            {
+                Console.WriteLine(temp1.data + " ");
+                temp1 = temp1.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ OrderdList/OrderdList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace OrderdList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\AKASH\source\repos\DataStructuredProblems2\OrderdList\OrderdList\data.txt";
+            string[] data = File.ReadAllLines(path);
+            LinkedList obj = new LinkedList();
+            Array.Sort(data);
+            foreach(string i in data)
+            {
+                obj.add(i);
+            }
+            obj.Display();
+        }
+    }
+}
